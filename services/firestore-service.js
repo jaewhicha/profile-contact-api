5708799e99a06674432af7b614089341baac0275
Jay Witcher <jay@jaywitcher.com>
2022-02-09 07:24:26 -0600

    Removed local config for firestore

--- services/firestore-service.js
+++ services/firestore-service.js
@@ -1,19 +1,7 @@
 const Firestore = require('@google-cloud/firestore')
-const env = require('../config/app.config')
 var uuid = require('node-uuid')
 
 const db = new Firestore();
-
-if (env.DEPLOY_ENV !== 'prod') {
-    db.settings({
-        projectId: 'dummy-contact-api',
-        host: '127.0.0.1',
-        port: 5050,
-        ssl: false,
-        keyFilename: '',
-        credentials: {}
-    });
-}
 
 const sumbmitContact = (contactInfo) => {
     const docRef = db.doc('contacts/' + uuid.v4());
