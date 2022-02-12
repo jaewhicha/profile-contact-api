94e9b21614e5d5e08d4b80aa9b489e0d58565403
Jay Witcher <jay@jaywitcher.com>
2022-02-12 03:40:16 -0600

    Fixing logger

--- services/logger-service.js
+++ services/logger-service.js
@@ -1,6 +1,8 @@
 const bunyan = require('bunyan');
 
-exports.logger = bunyan.createLogger({
+var logger = bunyan.createLogger({
     name: "jaewhicha-profile-contact-api",
     serializers: bunyan.stdSerializers,
-  });
+});
+
+module.exports = logger;
